d62358f678cef6525ab6ce2acaed9986ca3f7971
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-03 22:09:30 +0530

    positivennegative

---
+++ index.js
@@ -0,0 +1,8 @@
+function positiveandnegative(a) {
+    if (a < 0) {
+        console.log("its a negative no")
+    } else {
+        console.log("its a positive no")
+    }
+}
+positiveandnegative(99);
