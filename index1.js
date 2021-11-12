2fb80b8cbf0bb178810223655b9915f8cc10d6ba
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-12 15:53:12 +0530

    updated

---
+++ index1.js
@@ -0,0 +1,11 @@
+let b = 4;
+function leapyear(a) {
+    while (a <= 4) {
+        if (  a / b && a/4 == 0 ) {
+            console.log("its a leap year")
+        
+        }
+    
+    }
+}
+leapyear(2018);
