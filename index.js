f5dac6e7f8c7ca2c07fa00e0b511cb5b234dc5a8
srushti0415 <90923546+srushti0415@users.noreply.github.com>
2022-01-22 20:51:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function positiveandnegative(a) {
     if (a < 0) {
         console.log("its a negative no")
     } else {
-        console.log("its a positive no")
+        console.log("its a positive numnero")
     }
 }
 positiveandnegative(99);
